3dd1fcd9da59af6c15670f0eca14ac883c6f3b74
ahndal <ajs0904@gmail.com>
2018-06-03 22:05:18 +0900

    part2 completed(refactoring)

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,36 +1,74 @@
 <template>
   <div id="app">
     <todo-header></todo-header>
-    <todo-input></todo-input>
-    <todo-list></todo-list>
-    <todo-footer></todo-footer>
+    <todo-input v-on:addTodoItem="addOneItem"></todo-input>
+    <todo-list v-bind:propsdata="todoItems" v-on:removeTodoItem="removeOneItem" v-on:toggleTodoItem="toggleOneItem"></todo-list>
+    <todo-footer v-on:clearAll="clearAllItems"></todo-footer>
   </div>
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue';
-import TodoInput from './components/TodoInput.vue';
-import TodoList from './components/TodoList.vue';
-import TodoFooter from './components/TodoFooter.vue';
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
+  data: function() {
+    return {
+      todoItems: []
+    };
+  },
+  methods: {
+    addOneItem: function(todoItem) {
+      var obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+    removeOneItem: function(todoItem, index) {
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
+    },
+    toggleOneItem: function(todoItem, index) {
+      // todoItem.completed = !todoItem.completed;
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems: function() {
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+        }
+      }
+    }
+  },
   components: {
-    'todo-header': TodoHeader,
-    'todo-input': TodoInput,
-    'todo-list': TodoList,
-    'todo-footer': TodoFooter,
+    "todo-header": TodoHeader,
+    "todo-input": TodoInput,
+    "todo-list": TodoList,
+    "todo-footer": TodoFooter
   }
-}
+};
 </script>
 
 <style>
 body {
   text-align: center;
-  background: #F6F6F6;
+  background: #f6f6f6;
 }
 input {
   border-style: groove;
-  width:200px;
+  width: 200px;
 }
 button {
   border-style: groove;
